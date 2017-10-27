45da05837a12b82e879a77739f9daa2a2555693b
Feras Daeef <ferasit87@gmail.com>
2017-10-28 00:49:20 +0300

    organize

---
+++ example.php
@@ -0,0 +1,14 @@
+<?$APPLICATION->IncludeComponent(
+	"bitrix:user.list",
+	"",
+	Array(
+		"DATE_FORMAT" => "d.m.Y",
+		"DATE_TIME_FORMAT" => "d.m.Y H:i:s",
+		"PAGE_NAVIGATION_TEMPLATE" => "",
+		"SEO_USER" => "Y",
+		"SET_NAVIGATION" => "Y",
+		"SET_TITLE" => "Y",
+		"SHOW_USER_STATUS" => "N",
+		"USERS_PER_PAGE" => "2"
+	)
+);?>
